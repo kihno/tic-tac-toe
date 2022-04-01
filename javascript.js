cc7b686be5e8799ad273adce9eb335c603ef6bcf
kihno <stistyle13@gmail.com>
2022-04-01 16:46:39 -0700

    Add machineMove function for computer response to player move

--- javascript.js
+++ javascript.js
@@ -13,12 +13,56 @@ const gameBoard = (() => {
             if (tile.textContent === '') {
                 tile.textContent = 'X';
                 board.push('X');
+                machineMove().machineLogic();
             }
         });
     }
 
+    // function checkWinner() {
+    //     if ()
+    // }
+
     return {
+        tiles,
+        board,
         playerMove,
     }
 
 })();
+
+const player = (name) => {
+
+    return {
+        name,
+    }
+};
+
+const machine = player('The Machine');
+
+const machineMove = (() => {
+
+    const emptySpaces = [];
+
+    gameBoard.tiles.forEach(tile => {
+        if (tile.textContent === '') {
+            emptySpaces.push(tile)
+        }
+    })
+
+    function random() {
+        return emptySpaces[Math.floor(Math.random()*emptySpaces.length)];
+    }
+
+    function machineLogic() {
+        if (emptySpaces.length === 0) {
+            console.log('Tie Game');
+        } else {
+            random().textContent = 'O';
+            gameBoard.board.push('O');
+        }
+    }
+
+    return {
+        machineLogic,
+    }
+});
